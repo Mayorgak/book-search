14db746ee0c3062a5ca164232ffaeb93b9a69279
Kristen M <kristenmayorga@gmail.com>
2020-10-26 16:25:26 -0400

    deploying

--- server/server.js
+++ server/server.js
@@ -4,20 +4,26 @@ const { ApolloServer } = require("apollo-server-express");
 const path = require('path');
 const routes = require('./routes');
 const mongoose = require("mongoose");
-
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/book-search",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+
+
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
@@ -26,8 +32,6 @@ const server = new ApolloServer({
 
 
 server.applyMiddleware({ app });
-
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
